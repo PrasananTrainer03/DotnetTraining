799bda7474decfe4d982fc8d3ad06ade618f1d9e
Prasanna <prassup.dotnet@gmail.com>
2021-05-15 21:43:33 +0530

    code

---
+++ dotnet_nandana/Day5/Ef2/Ef2/EmpSearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ef2
+{
+    class EmpSearch
+    {
+        static void Main()
+        {
+            sqlpracticeEntities entites = new sqlpracticeEntities();
+            int empno;
+            Console.WriteLine("Enter Employ No   ");
+            empno = Convert.ToInt32(Console.ReadLine());
+            EMp result = entites.EMps.Where(x => x.Empno == empno).FirstOrDefault<EMp>();
+            Console.WriteLine(result.Empno);
+            Console.WriteLine(result.nam);
+            Console.WriteLine(result.dept);
+        }
+    }
+}
---
+++ dotnet_nandana/Day5/Ef2/Ef2/EmployAdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ef2
+{
+    class EmployAdd
+    {
+        static void Main()
+        {
+            EMp emp = new EMp();
+            Console.WriteLine("Enter Employ No    ");
+            emp.Empno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employ Name   ");
+            emp.nam = Console.ReadLine();
+            Console.WriteLine("Enter Department  ");
+            emp.dept = Console.ReadLine();
+            Console.WriteLine("Enter Designation  ");
+            emp.desig = Console.ReadLine();
+            Console.WriteLine("Enter Basic   ");
+            emp.basic = Convert.ToInt32(Console.ReadLine());
+            sqlpracticeEntities entities = new sqlpracticeEntities();
+            entities.EMps.Add(emp);
+            entities.SaveChanges();
+            Console.WriteLine("*** Record Inserted ***");
+        }
+    }
+}
